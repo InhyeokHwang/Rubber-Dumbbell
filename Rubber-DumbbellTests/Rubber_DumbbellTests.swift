e79da463b4b55eeb1de1aa82ec4742f8727758b4
inhyeok <inhyeok@inhyeokui-MacBookPro.local>
2025-08-04 19:45:16 +0900

    initial commit

---
+++ Rubber-DumbbellTests/Rubber_DumbbellTests.swift
@@ -0,0 +1,17 @@
+//
+//  Rubber_DumbbellTests.swift
+//  Rubber-DumbbellTests
+//
+//  Created by inhyeok on 8/4/25.
+//
+
+import Testing
+@testable import Rubber_Dumbbell
+
+struct Rubber_DumbbellTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
